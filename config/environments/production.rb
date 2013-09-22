7bebe081bb9e3e242074fef666c8f6f4b38d8cb1
Octavio <octanks@hotmail.com>
2013-09-22 12:30:17 -0300

    ignorar assets de spree local

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,10 +43,11 @@ OrbitalGlasses::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "http://nuevediez.com"
+  # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  #config.assets.precompile += %w( search.js store/all.js store/all.css )
 
 
   # Disable delivery errors, bad email addresses will be ignored
