454f8e0c50b1ae891d42b602c08c5d3cb7bdb416
Octavio <octanks@hotmail.com>
2013-09-26 14:51:36 -0300

    filtro de taxons solo para la seccion actual

--- app/controllers/spree/types_controller.rb
+++ app/controllers/spree/types_controller.rb
@@ -6,10 +6,26 @@ module Spree
     end
 
     def index
-      @types = Spree::Taxon.find_by_name!(params[:types])
-      @products = Spree::Product.in_taxon(@types)
-      @taxons = Spree::Taxon.all
-      @taxonomy = Spree::Taxonomy.all
+      if (params.has_key?(:types))
+        @types = Spree::Taxon.find_by_permalink!(params[:types])
+        @products = Spree::Product.in_taxon(@types)
+        @taxons = Spree::Taxon.where('permalink like ?', "%#{params[:types]}%")
+      else
+        @products = Array.new
+        @product_taxon = Hash.new
+
+        @filter = Spree::Taxon.where('permalink like ?', "%#{params[:filter]}%")
+        @filter.each do |filter|
+          @product_taxon = ActiveRecord::Base.connection.select_all("SELECT * FROM spree_products_taxons WHERE taxon_id = #{filter.id}");
+          unless @product_taxon.empty?
+            @product_taxon.each do |tax|
+              @lala = tax[:product_id]
+              @products += Spree::Product.find(:all, :conditions => {:id => tax[:product_id]})
+            end
+          end
+        end
+        @taxons = Spree::Taxon.where('permalink like ?', "%#{params[:filter]}%")
+      end
     end
   end
 end
